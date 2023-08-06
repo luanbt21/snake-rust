a45fe833431e5e359146c362202757fa7dcd668e
luanbt21 <luanbt21@gmail.com>
2023-08-06 15:37:15 +0700

    Completed

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod draw;
+mod game;
+mod snake;
+
+extern crate piston_window;
+extern crate rand;
+
+use piston_window::types::Color;
+use piston_window::{clear, Button, PistonWindow, PressEvent, UpdateEvent, WindowSettings};
+
+use crate::draw::to_coord_u32;
+use crate::game::Game;
+
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+fn main() {
+    let (width, height) = (20, 20);
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
+            .exit_on_esc(true)
+            .build()
+            .unwrap();
+
+    let mut game = Game::new(width, height);
+
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg| {
+            game.update(arg.dt);
+        });
+    }
+}
